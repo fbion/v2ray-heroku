60fb830fde67fcd7148a6ec3aad206285b0a5ce4
fbion <6179189+fbion@users.noreply.github.com>
2018-05-07 22:38:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 cd /v2raybin
 wget -O v2ray.tar.gz https://github.com/ginuerzh/gost/releases/download/v2.5/gost_2.5_linux_amd64.tar.gz
-tar xvf v2ray.tar.gz 
+tar zxf v2ray.tar.gz 
 cd /v2raybin/gost_2.5_linux_amd64
 chmod +x gost
 
-./gost -L mwss://:8443
+./gost -L mwss://:"$PORT"
